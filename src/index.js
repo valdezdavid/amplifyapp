c65c53f0a6b243711a67434bb48a2181660bc94d
David <valdezd@usc.edu>
2020-12-21 21:55:13 -0800

    Removed boilerplate React ui

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ Amplify.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
-  <h1>Hello World!</h1>
+  <h1>Fuego Bot</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
